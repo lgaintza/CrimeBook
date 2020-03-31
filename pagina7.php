53816c0e40f408642a39aabac3b62e5d6b4e3cef
lgaintza <luii.sa93@gmail.com>
2020-03-31 18:00:17 +0200

    mejoras en la pagina 7
    Todavia me falta modificar la pagina 8para recibir lo de yolanda. Ahora no funciona el formulario

--- pagina7.php
+++ pagina7.php
@@ -3,32 +3,25 @@
     require_once('include/Estadistica.php');
     require_once('Smarty.class.php');
 
-    // Recuperamos la información de la sesión
-    session_start();
+   // Recuperamos la información de la sesión
+   session_start();
 
-    // Y comprobamos que el usuario se haya autentificado
-    if (!isset($_SESSION['usuario'])) 
-        die("Error - debe <a href='login.php'>identificarse</a>.<br />");
-        else {
-            // $idJuego = $_SESSION['idJuego'];
-            
-        }
-    // Cargamos la librería de Smarty
-    $smarty = new Smarty;
-    $smarty->template_dir = 'smarty/templates/';
-    $smarty->compile_dir = 'smarty/templates_c/';
-    $smarty->config_dir = 'smarty/configs/';
-    $smarty->cache_dir = 'smarty/cache/';
+   // Y comprobamos que el usuario se haya autentificado
 
-   
+   // Cargamos la librería de Smarty
+   $smarty = new Smarty;
+   $smarty->template_dir = 'smarty/templates/';
+   $smarty->compile_dir = 'smarty/templates_c/';
+   $smarty->config_dir = 'smarty/configs/';
+   $smarty->cache_dir = 'smarty/cache/';
 
-    $juegos = BD::obtieneEstadistica(200001);
-    $estadisticas = new Estadistica($juegos);
-  
 
-   $smarty->assign('estadisticas', $estadisticas);
-    // Mostramos la plantilla
-    $smarty->display('estadisticas_pag7.tpl');   
-    //print_r($estadisticas);
-  
+
+   $juegos = BD::obtieneEstadistica(200001);
+
+  $smarty->assign('juegoest',$juegos);
+   // Mostramos la plantilla
+   $smarty->display('estadisticas_pag7.tpl');   
+   //print_r($estadisticas);
+
 ?>
--- pagina8.php
+++ pagina8.php
@@ -18,27 +18,31 @@ $smarty->cache_dir = 'smarty/cache/';
 
 // Comprobamos si ya se ha enviado el formulario
 if (isset($_POST['anadirPista'])){
-    $idPrueba =$_POST['codigoPrueba'];
+    //$_POST['codigoPrueba'];
 
 }
+ $idPrueba = 400001;
+ $_SESSION['idPrueba'] = $idPrueba;
 if (isset($_POST['guardar'])) {
-    $idPrueba=$_POST['idPrueba'];
+    
     $id=$_POST['id'];
     $texto = $_POST['texto'];
     $tiempo = $_POST['tiempo'];
     $intentos = $_POST['intentos'];
  
-   
-    $pista = BD::creaPista($idPrueba, $id, $texto, $tiempo, $intentos);
+   echo ($_SESSION['idPrueba'].$id.$tiempo.$intentos);
+    $pista = BD::creaPista($_SESSION['idPrueba'], $id, $texto, $tiempo, $intentos);
 
     //var_dump($pista);
-    header("Location: pagina6.php");
+  //  header("Location: pagina6.php");
 
       
 }
 if(isset($_POST['cancelar'])){
     header("Location: pagina6.php");
 }
+
+$smarty->assign('idPrueba', $idPrueba);
 // Mostramos la plantilla
 $smarty->display('crearPista_pag8.tpl');
 ?>
